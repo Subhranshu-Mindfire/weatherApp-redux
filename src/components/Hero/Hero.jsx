421c72fc5da221a438bd235c45d6944d1a69eaf8
subhranshu <subhranshu.mfs@gmail.com>
2025-03-28 15:20:32 +0530

    5: Added Top  Component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./Hero.css"
+import Top from '../Top/Top'
 
 const Hero = () => {
   return (
     <div className='hero h-75 w-75'>
+      <Top/>
     </div>
   )
 }
---
+++ src/components/Top/Top.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Top = () => {
+  return (
+    <div className='d-flex justify-content-between p-5 top'>
+      <div className="left">
+        <div className="place h1 font-monospace text-white">
+          Bhubaneswar
+        </div>
+        <div className="h5 text-white">
+          Friday, 28 March 2025 | 01 : 11 PM
+        </div>
+      </div>
+      <div className="right">
+        <input type="text" className='form-control fs-4' placeholder='Search for city'/>
+      </div>
+    </div>
+  )
+}
+
+export default Top
