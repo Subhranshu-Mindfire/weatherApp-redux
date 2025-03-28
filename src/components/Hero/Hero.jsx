2a7bf3b21b752d1b3acf92a2e3392ca095e68732
subhranshu <subhranshu.mfs@gmail.com>
2025-03-28 15:14:06 +0530

    5: Added Hero  Component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Hero.css"
+
+const Hero = () => {
+  return (
+    <div className='hero h-75 w-75'>
+    </div>
+  )
+}
+
+export default Hero
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./Layout.css"
+import Hero from '../Hero/Hero'
 
 const Layout = () => {
   return (
-    <div className='vh-100 layout'>
+    <div className='vh-100 layout d-flex justify-content-center align-items-center'>
+      <Hero/>
     </div>
   )
 }
